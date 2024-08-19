023422d562a9c4e53738df6f79c64ffd9ae36d77
thi1604 <thibeo1604@gmail.com>
2024-08-19 14:47:27 +0700

    Lam giao dien requestFriend

--- controller/client/users.controller.js
+++ controller/client/users.controller.js
@@ -2,17 +2,20 @@ const userModel = require("../../models/user.model");
 const usersSocket = require("../../socket/users.socket");
 
 
+// Danh sách người dùng
+
+
 module.exports.notFriend = async (req, res) => {
   const idUser = res.locals.user.id;
-
+  
   const requestFriends = res.locals.user.requestFriends;
   const acceptFriends = res.locals.user.acceptFriends;
   const friendsList = res.locals.user.friendsList;
   const friendsListId = friendsList.map(item => item.userId);
-
-
+  
+  
   await usersSocket(req, res);
-
+  
   const listNotFriend = await userModel.find({
     $and: [
       {_id: {$ne: idUser}},
@@ -29,3 +32,29 @@ module.exports.notFriend = async (req, res) => {
     listNotFriend: listNotFriend
   });
 }
+//End Danh sách người dùng
+
+
+//Danh sách yêu cầu kết bạn
+module.exports.request = async (req, res) => {
+
+  const requestFriends = res.locals.user.requestFriends;
+ 
+  await usersSocket(req, res);
+  
+  const listRequestFriend = await userModel.find({
+    _id: {
+      $in: requestFriends
+    },
+    status: "active",
+    deleted: false
+  }).select("fullName id avatar");
+
+  res.render("client/pages/users/request.pug", {
+    pageTitle: "Trang yêu cầu kết bạn",
+    users: listRequestFriend
+  })
+
+
+}
+//End Danh sách yêu cầu kết bạn
--- routes/client/users.route.js
+++ routes/client/users.route.js
@@ -4,4 +4,7 @@ const controller = require("../../controller/client/users.controller.js");
 
 router.get("/not-friend", controller.notFriend);
 
+
+router.get("/request", controller.request);
+
 module.exports = router;
